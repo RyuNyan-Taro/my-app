6c86a44ff75369921f8408a63e7e5e7ef9f4a2a9
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-04-07 14:25:14 +0900

    create chat api

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse, NextRequest } from "next/server";
+
+let chatMessages = [
+    {
+        id: 1,
+        role: "bot", 
+        message: "こんにちは！",
+    },
+];
+
+export async function GET() {
+    return NextResponse.json(chatMessages)
+}
+
+export async function POST(request: NextRequest) {
+    const data = await request.json();
+
+    const newMessage = {
+        id: chatMessages.length + 1,
+        role: data.role || "user",
+        message: data.message,
+    };
+
+    chatMessages.push(newMessage);
+
+    chatMessages.push({
+        id: chatMessages.length + 1,
+        role: "bot",
+        message: data.message,
+    });
+
+    return NextResponse.json(newMessage)
+
+}
+
